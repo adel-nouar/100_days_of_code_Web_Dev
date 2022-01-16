45301d03c95f3a1dc4e25e58e62f9778ec97ffb4
Adel NOUAR <adel.nouar@protonmail.com>
2022-01-16 19:43:10 +0100

    Day30: grouping related data

--- Section_11/app.js
+++ Section_11/app.js
@@ -2,5 +2,7 @@ let age = 32;
 let userName = "Adel";
 let hobbies = ["Sports", "Cooking", "Reading"];
 
+let job = { title: "Developer", place: "New York", salary: "5000000" };
+
 alert(hobbies);
 alert(hobbies[1]);
