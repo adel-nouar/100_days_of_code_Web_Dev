ca467085c610b902d94a8f6079efdf29ec73a46f
Adel NOUAR <adel.nouar@protonmail.com>
2022-01-17 12:15:52 +0100

    Day31: Splitting code into multiple lines

--- Section_11/app.js
+++ Section_11/app.js
@@ -2,7 +2,11 @@ let age = 32;
 let userName = "Adel";
 let hobbies = ["Sports", "Cooking", "Reading"];
 
-let job = { title: "Developer", place: "New York", salary: "5000000" };
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: "5000000",
+};
 
 alert(hobbies);
 alert(hobbies[1]);
