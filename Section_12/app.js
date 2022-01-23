79b8c4081e2f0bca7f73d88f66df8b44eb9ca793
Adel NOUAR <adel.nouar@protonmail.com>
2022-01-23 20:56:46 +0100

    Day34: searching DOM elements

--- Section_12/app.js
+++ Section_12/app.js
@@ -1,3 +1,6 @@
 // console.dir(document);
 
-document.body.children[1].children[0].href = "https://google.com";
+// document.body.children[1].children[0].href = "https://google.com";
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
