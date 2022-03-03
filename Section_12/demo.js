93054f722441dd55d88c91667830c8eb1f1b2d0b
Adel NOUAR <adel.nouar@protonmail.com>
2022-03-03 20:48:03 +0100

    Day36: intro to constant

--- Section_12/demo.js
+++ Section_12/demo.js
@@ -1,13 +1,13 @@
-let productNameInputElement = document.getElementById("product-name");
-let remainingCharsElement = document.getElementById("remaining-chars");
+const productNameInputElement = document.getElementById("product-name");
+const remainingCharsElement = document.getElementById("remaining-chars");
 
-let maxAllowedChars = productNameInputElement.maxLength;
+const maxAllowedChars = productNameInputElement.maxLength;
 
 function updateRemainingCharacters(event) {
-  let enteredText = event.target.value;
-  let enteredTextLength = enteredText.length;
+  const enteredText = event.target.value;
+  const enteredTextLength = enteredText.length;
 
-  let remainingCharacters = maxAllowedChars - enteredTextLength;
+  const remainingCharacters = maxAllowedChars - enteredTextLength;
 
   remainingCharsElement.textContent = remainingCharacters;
 }
