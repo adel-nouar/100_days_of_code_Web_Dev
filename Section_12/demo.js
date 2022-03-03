6c8368f4b6b14abb694f82fdd99f9637c6e0b2a8
Adel NOUAR <adel.nouar@protonmail.com>
2022-03-03 20:40:17 +0100

    Day36: Demo

---
+++ Section_12/demo.js
@@ -0,0 +1,15 @@
+let productNameInputElement = document.getElementById("product-name");
+let remainingCharsElement = document.getElementById("remaining-chars");
+
+let maxAllowedChars = productNameInputElement.maxLength;
+
+function updateRemainingCharacters(event) {
+  let enteredText = event.target.value;
+  let enteredTextLength = enteredText.length;
+
+  let remainingCharacters = maxAllowedChars - enteredTextLength;
+
+  remainingCharsElement.textContent = remainingCharacters;
+}
+
+productNameInputElement.addEventListener("input", updateRemainingCharacters);
