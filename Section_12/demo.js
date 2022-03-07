9a773852bd595281e3ef09c7261d4cee7a45d6d1
Adel NOUAR <adel.nouar@protonmail.com>
2022-03-07 21:39:00 +0100

    Day38: add class list to remaining chars

--- Section_12/demo.js
+++ Section_12/demo.js
@@ -10,6 +10,11 @@ function updateRemainingCharacters(event) {
   const remainingCharacters = maxAllowedChars - enteredTextLength;
 
   remainingCharsElement.textContent = remainingCharacters;
+
+  if (remainingCharacters <= 10) {
+    remainingCharsElement.classList.add("warning");
+    productNameInputElement.classList.add("warning");
+  }
 }
 
 productNameInputElement.addEventListener("input", updateRemainingCharacters);
