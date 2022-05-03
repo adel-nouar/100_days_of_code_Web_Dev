dabd6fb492f970d782cce6cd5d2565281fc05eea
Adel NOUAR <adel.nouar@protonmail.com>
2022-05-03 15:56:50 +0200

    Day38: else and els if

--- Section_12/demo.js
+++ Section_12/demo.js
@@ -11,9 +11,25 @@ function updateRemainingCharacters(event) {
 
   remainingCharsElement.textContent = remainingCharacters;
 
-  if (remainingCharacters <= 10) {
+  // if (remainingCharacters <= 10) {
+  //   remainingCharsElement.classList.add("warning");
+  //   productNameInputElement.classList.add("warning");
+  // } else {
+  //   remainingCharsElement.classList.remove("warning");
+  //   productNameInputElement.classList.remove("warning");
+  // }
+
+  if (remainingCharacters === 0) {
+    remainingCharsElement.classList.add("error");
+    productNameInputElement.classList.add("error");
+  } else if (remainingCharacters <= 10) {
     remainingCharsElement.classList.add("warning");
     productNameInputElement.classList.add("warning");
+    remainingCharsElement.classList.remove("error");
+    productNameInputElement.classList.remove("error");
+  } else {
+    remainingCharsElement.classList.remove("warning");
+    productNameInputElement.classList.remove("warning");
   }
 }
 
