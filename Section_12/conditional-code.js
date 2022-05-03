2091dc0064629b7b6cd2b99bf06e393979e33452
Adel NOUAR <adel.nouar@protonmail.com>
2022-05-03 17:13:56 +0200

    Day38: Done

--- Section_12/conditional-code.js
+++ Section_12/conditional-code.js
@@ -3,7 +3,14 @@ const myName = "Max";
 if (myName === "Max") {
   console.log("Hello");
 }
+let isLoggedIn = true;
 
-if ("Max") {
-  console.log("Hello");
+if (!isLoggedIn) {
+  console.log("User is NOT logged in!");
+}
+
+const enteredUserName = "";
+
+if (enteredUserName) {
+  console.log("Input is valid!");
 }
