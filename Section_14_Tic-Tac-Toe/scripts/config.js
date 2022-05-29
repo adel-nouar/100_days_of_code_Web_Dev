0823315f2f0ddf91ef69b641758404b607a7937e
Adel NOUAR <adel.nouar@protonmail.com>
2022-05-30 00:24:30 +0200

    [41] JS - show & hide modal

--- Section_14_Tic-Tac-Toe/scripts/config.js
+++ Section_14_Tic-Tac-Toe/scripts/config.js
@@ -1,1 +1,9 @@
-function openPlayerConfig() {}
+function openPlayerConfig() {
+    playerConfigOverlayElement.style.display = "block";
+    backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = "none";
+    backdropElement.style.display = "none";
+}
