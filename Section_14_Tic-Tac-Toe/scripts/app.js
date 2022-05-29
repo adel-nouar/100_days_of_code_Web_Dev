b36eda22c145d06660c3b40ccdbd9d504f789eb5
Adel NOUAR <adel.nouar@protonmail.com>
2022-05-30 00:15:10 +0200

    [41] JS - evt listener players

---
+++ Section_14_Tic-Tac-Toe/scripts/app.js
@@ -0,0 +1,5 @@
+const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
+const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
+
+editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
+editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
