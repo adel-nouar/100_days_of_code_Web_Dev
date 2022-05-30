fceecf2088cff4089e27d5462832f057a6adb5ba
Adel NOUAR <adel.nouar@protonmail.com>
2022-05-30 21:33:38 +0200

    [42] JS - Form

--- Section_14_Tic-Tac-Toe/scripts/app.js
+++ Section_14_Tic-Tac-Toe/scripts/app.js
@@ -1,5 +1,6 @@
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
@@ -10,3 +11,5 @@ editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
--- Section_14_Tic-Tac-Toe/scripts/config.js
+++ Section_14_Tic-Tac-Toe/scripts/config.js
@@ -7,3 +7,9 @@ function closePlayerConfig() {
     playerConfigOverlayElement.style.display = "none";
     backdropElement.style.display = "none";
 }
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get("playername");
+}
