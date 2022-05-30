62d496a1133e22fc9ee63839b037063b8b762ea8
Adel NOUAR <adel.nouar@protonmail.com>
2022-05-30 22:03:13 +0200

    [42] JS - Validating user input

--- Section_14_Tic-Tac-Toe/scripts/app.js
+++ Section_14_Tic-Tac-Toe/scripts/app.js
@@ -1,6 +1,7 @@
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
--- Section_14_Tic-Tac-Toe/scripts/config.js
+++ Section_14_Tic-Tac-Toe/scripts/config.js
@@ -6,10 +6,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = "none";
     backdropElement.style.display = "none";
+    formElement.firstElementChild.classList.remove("error");
+    errorsOutputElement.textContent = "";
 }
 
 function savePlayerConfig(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const enteredPlayername = formData.get("playername");
+    const enteredPlayername = formData.get("playername").trim();
+
+    if (!enteredPlayername) {
+        event.target.firstElementChild.classList.add("error");
+        errorsOutputElement.textContent = "Please enter a valid name!";
+        return;
+    }
 }
