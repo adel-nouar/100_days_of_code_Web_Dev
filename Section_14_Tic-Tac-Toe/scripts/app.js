6c03c743cdf56e99f42dbab4df5c062b00106495
Adel NOUAR <adel.nouar@protonmail.com>
2022-05-31 14:29:55 +0200

    [43] JS - Game rounds and selection

--- Section_14_Tic-Tac-Toe/scripts/app.js
+++ Section_14_Tic-Tac-Toe/scripts/app.js
@@ -1,4 +1,5 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
     {
@@ -16,11 +17,14 @@ const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
 const gameAreaElement = document.getElementById("active-game");
+const activePlayerNameElement = document.getElementById("active-player-name");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 const startNewGameBtnElement = document.getElementById("start-game-btn");
+// const gameFieldElements = document.querySelectorAll("#game-board li");
+const gameBoardElement = document.getElementById("game-board");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -31,3 +35,9 @@ backdropElement.addEventListener("click", closePlayerConfig);
 formElement.addEventListener("submit", savePlayerConfig);
 
 startNewGameBtnElement.addEventListener("click", startNewGame);
+
+// for (const gameFieldElement of gameFieldElements) {
+//     gameFieldElement.addEventListener("click", selectGameField);
+// }
+
+gameAreaElement.addEventListener("click", selectGameField);
--- Section_14_Tic-Tac-Toe/scripts/game.js
+++ Section_14_Tic-Tac-Toe/scripts/game.js
@@ -3,5 +3,23 @@ function startNewGame() {
         alert("Please set custom player names for both players!");
         return;
     }
+    activePlayerNameElement.textContent = players[activePlayer].name;
     gameAreaElement.style.display = "block";
+}
+
+function switchPlayer() {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+    activePlayerNameElement.textContent = players[activePlayer].name;
+}
+function selectGameField(event) {
+    if (event.target.tagName !== "LI") {
+        return;
+    }
+    event.target.textContent = players[activePlayer].symbol;
+    event.target.classList.add("disabled");
+    switchPlayer();
 }
