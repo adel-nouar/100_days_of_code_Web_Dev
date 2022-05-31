57d6e5b394a4008620244e058cbbd3027beda72a
Adel NOUAR <adel.nouar@protonmail.com>
2022-05-31 15:46:06 +0200

    [43] JS - tracking selected fields

--- Section_14_Tic-Tac-Toe/scripts/app.js
+++ Section_14_Tic-Tac-Toe/scripts/app.js
@@ -1,3 +1,8 @@
+const gameData = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
 let editedPlayer = 0;
 let activePlayer = 0;
 
--- Section_14_Tic-Tac-Toe/scripts/game.js
+++ Section_14_Tic-Tac-Toe/scripts/game.js
@@ -19,7 +19,20 @@ function selectGameField(event) {
     if (event.target.tagName !== "LI") {
         return;
     }
-    event.target.textContent = players[activePlayer].symbol;
-    event.target.classList.add("disabled");
+
+    const selectedField = event.target;
+    const selectedColumn = selectedField.dataset.col - 1;
+    const selectedRow = selectedField.dataset.row - 1;
+
+    if (gameData[selectedRow][selectedColumn] > 0) {
+        alert("Please select an empty field");
+        return;
+    }
+    selectedField.textContent = players[activePlayer].symbol;
+    selectedField.classList.add("disabled");
+
+    gameData[selectedRow][selectedColumn] = activePlayer + 1;
+    console.log(gameData);
+
     switchPlayer();
 }
