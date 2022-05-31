e1966f353bacf8d0e4dd547627137adf859da748
Adel NOUAR <adel.nouar@protonmail.com>
2022-05-31 12:11:08 +0200

    [43] JS - Logic starting game

--- Section_14_Tic-Tac-Toe/scripts/app.js
+++ Section_14_Tic-Tac-Toe/scripts/app.js
@@ -15,10 +15,12 @@ const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
+const gameAreaElement = document.getElementById("active-game");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
+const startNewGameBtnElement = document.getElementById("start-game-btn");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -27,3 +29,5 @@ cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
 
 formElement.addEventListener("submit", savePlayerConfig);
+
+startNewGameBtnElement.addEventListener("click", startNewGame);
