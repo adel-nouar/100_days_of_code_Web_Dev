ca6ebdd4431baa8e2eb919c56387ee9f9ecf00b5
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-03 11:43:24 +0200

    [49] node.js express + nodemon

--- Section_17/app.js
+++ Section_17/app.js
@@ -13,7 +13,7 @@ app.get("/currenttime", function (req, res) {
 
 app.get("/", function (req, res) {
     res.send(
-        "<form action='/store-user' method='POST'><label>Your Name</label> <input type='text' name='username'><button>Submit</button></form>"
+        "<form action='/store-user' method='POST'><label>Your Name: </label> <input type='text' name='username'><button>Submit</button></form>"
     );
 });
 
@@ -29,6 +29,23 @@ app.post("/store-user", function (req, res) {
     fs.writeFileSync(filePath, JSON.stringify(existingUsers));
 
     res.send("<h1>Username stored!</h1>");
+});
+
+app.get("/users", function (req, res) {
+    const filePath = path.join(__dirname, "data", "users.json");
+
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    let responseData = "<ul>";
+
+    for (const user of existingUsers) {
+        responseData += "<li>" + user + "</li>";
+    }
+
+    responseData += "</ul>";
+
+    res.send(responseData);
 });
 
 app.listen(3000);
