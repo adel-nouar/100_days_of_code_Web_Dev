6c076d26b26ff1dded51cab07b6427987009bb0d
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-04 20:12:55 +0200

    [52] Refactoring code

--- Section_19_and_20/project/app.js
+++ Section_19_and_20/project/app.js
@@ -39,7 +39,7 @@ app.get("/restaurants/:id", function (req, res) {
             return res.render("restaurant-detail", { restaurant: restaurant });
         }
     }
-    res.render("404");
+    res.status(404).render("404");
 });
 
 app.get("/recommend", function (req, res) {
@@ -50,13 +50,11 @@ app.post("/recommend", function (req, res) {
     const restaurant = req.body;
     restaurant.id = uuid.v4();
 
-    const filePath = path.join(__dirname, "data", "restaurants.json");
-    const fileData = fs.readFileSync(filePath);
-    const storedRestaurants = JSON.parse(fileData);
+    const restaurants = getStoredRestaurant();
 
-    storedRestaurants.push(restaurant);
+    restaurants.push(restaurant);
 
-    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+    storeRestaurants(restaurants);
 
     res.redirect("/confirm");
 });
@@ -67,6 +65,14 @@ app.get("/confirm", function (req, res) {
 
 app.get("/about", function (req, res) {
     res.render("about");
+});
+
+app.use(function (req, res) {
+    res.status(404).render("404");
+});
+
+app.use(function (error, req, res, next) {
+    res.status(500).render("500");
 });
 
 app.listen(3000);
