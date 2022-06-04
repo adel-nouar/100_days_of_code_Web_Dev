c11e75dc623e202e9768a06510b3ffc594d448b9
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-04 17:40:57 +0200

    [51] EJS - dynamic route

--- Section_19_and_20/project/app.js
+++ Section_19_and_20/project/app.js
@@ -26,6 +26,11 @@ app.get("/restaurants", function (req, res) {
     });
 });
 
+app.get("/restaurants/:id", function (req, res) {
+    const restaurantId = req.params.id;
+    res.render("restaurant-detail", { rid: restaurantId });
+});
+
 app.get("/recommend", function (req, res) {
     res.render("recommend");
 });
