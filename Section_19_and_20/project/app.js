e203e31dd55a35a9613926a0d9cca0a2902699a6
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-05 15:53:35 +0200

    [53] Refactor code: split routes into different files

--- Section_19_and_20/project/app.js
+++ Section_19_and_20/project/app.js
@@ -1,10 +1,9 @@
-const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
-const uuid = require("uuid");
 
-const resData = require("./util/restaurant-data");
+const defaultRoutes = require("./routes/default");
+const restaurantRoutes = require("./routes/restaurants");
 
 const app = express();
 
@@ -14,55 +13,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", function (req, res) {
-    res.render("index");
-});
-
-app.get("/restaurants", function (req, res) {
-    const storedRestaurants = resData.getStoredRestaurants();
-
-    res.render("restaurants", {
-        numberOfRestaurants: storedRestaurants.length,
-        restaurants: storedRestaurants,
-    });
-});
-
-app.get("/restaurants/:id", function (req, res) {
-    const restaurantId = req.params.id;
-    const storedRestaurants = resData.getStoredRestaurants();
-
-    for (const restaurant of storedRestaurants) {
-        if (restaurant.id === restaurantId) {
-            return res.render("restaurant-detail", { restaurant: restaurant });
-        }
-    }
-    res.status(404).render("404");
-});
-
-app.get("/recommend", function (req, res) {
-    res.render("recommend");
-});
-
-app.post("/recommend", function (req, res) {
-    const restaurant = req.body;
-    restaurant.id = uuid.v4();
-
-    const restaurants = resData.getStoredRestaurants();
-
-    restaurants.push(restaurant);
-
-    resData.storeRestaurants(restaurants);
-
-    res.redirect("/confirm");
-});
-
-app.get("/confirm", function (req, res) {
-    res.render("confirm");
-});
-
-app.get("/about", function (req, res) {
-    res.render("about");
-});
+app.use("/", defaultRoutes);
+app.use("/", restaurantRoutes);
 
 app.use(function (req, res) {
     res.status(404).render("404");
