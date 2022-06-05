3a5a7fa6ecf794bac62225acd29f56128c6b52f0
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-05 23:17:45 +0200

    [53] Query parameters for sorting restaurants

--- Section_19_and_20/project/routes/restaurants.js
+++ Section_19_and_20/project/routes/restaurants.js
@@ -7,11 +7,33 @@ const resData = require("../util/restaurant-data");
 const router = express.Router();
 
 router.get("/restaurants", function (req, res) {
+    let order = req.query.order;
+    let nextOrder = "desc";
+
+    if (order !== "asc" && order !== "desc") {
+        order = "asc";
+    }
+
+    if (order === "desc") {
+        nextOrder = "asc";
+    }
+
     const storedRestaurants = resData.getStoredRestaurants();
+
+    storedRestaurants.sort(function (resA, resB) {
+        if (
+            (order === "asc" && resA.name > resB.name) ||
+            (order === "desc" && resB.name > resA.name)
+        ) {
+            return 1;
+        }
+        return -1;
+    });
 
     res.render("restaurants", {
         numberOfRestaurants: storedRestaurants.length,
         restaurants: storedRestaurants,
+        nextOrder: nextOrder,
     });
 });
 
