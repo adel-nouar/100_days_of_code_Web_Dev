87433aa4f5966828330cd6fdf18eb70882e340df
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-06 16:14:12 +0200

    [54] - function spread operator

--- Section_21/functions.js
+++ Section_21/functions.js
@@ -4,3 +4,16 @@ function greetUser(greetingPrefix, userName = "user") {
 
 greetUser("Hi", "Test");
 greetUser("Hello");
+
+function sumUp(...numbers) {
+    let result = 0;
+
+    for (const number of numbers) {
+        result += number;
+    }
+    return result;
+}
+
+const inputNumbers = [1, 5, 10, 11, 20, 31];
+
+console.log(sumUp(...inputNumbers));
