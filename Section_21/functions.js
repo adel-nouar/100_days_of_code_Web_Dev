ddac7c1490ce08aa2a373e75c6fc3a98c0d1619f
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-06 16:22:34 +0200

    [54] - Template literal

--- Section_21/functions.js
+++ Section_21/functions.js
@@ -1,5 +1,6 @@
 function greetUser(greetingPrefix, userName = "user") {
     console.log(greetingPrefix + " " + userName + "!");
+    console.log(`${greetingPrefix} ${userName}!`);
 }
 
 greetUser("Hi", "Test");
@@ -17,3 +18,5 @@ function sumUp(...numbers) {
 const inputNumbers = [1, 5, 10, 11, 20, 31];
 
 console.log(sumUp(...inputNumbers));
+
+console.log(sumUp);
