b1401b2fba1df9dca0c97c470394ed875ed16725
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-08 14:30:39 +0200

    [56] - Promises

--- Section_21/async.js
+++ Section_21/async.js
@@ -1,8 +1,12 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
     let fileData;
-    fs.readFile("data.txt", function (error, fileData) {
+    // fs.readFile("data.txt", function (error, fileData) {
+    //     console.log("File parsing done!");
+    //     console.log(fileData.toString());
+    // });
+    fs.readFile("data.txt").then(function (fileData) {
         console.log("File parsing done!");
         console.log(fileData.toString());
     });
