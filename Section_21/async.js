28881bbb2b79022204982bb320ce28608dbf8e84
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-08 14:34:18 +0200

    [56] - Error in async

--- Section_21/async.js
+++ Section_21/async.js
@@ -3,13 +3,21 @@ const fs = require("fs/promises");
 function readFile() {
     let fileData;
     // fs.readFile("data.txt", function (error, fileData) {
+    // if (error) {
+    // }
     //     console.log("File parsing done!");
     //     console.log(fileData.toString());
     // });
-    fs.readFile("data.txt").then(function (fileData) {
-        console.log("File parsing done!");
-        console.log(fileData.toString());
-    });
+    fs.readFile("data.txt")
+        .then(function (fileData) {
+            console.log("File parsing done!");
+            console.log(fileData.toString());
+            // return anotherAsyncOperation;
+        })
+        .then(function () {})
+        .catch(function (error) {
+            console.log(error);
+        });
     console.log("Hi there!");
 }
 
