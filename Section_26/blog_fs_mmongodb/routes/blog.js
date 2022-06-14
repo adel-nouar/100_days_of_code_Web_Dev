f96ad76829f36afb7bb2c22f678e195abc864cd8
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-14 23:43:02 +0200

    [66] - blog mongdb - deleting post

--- Section_26/blog_fs_mmongodb/routes/blog.js
+++ Section_26/blog_fs_mmongodb/routes/blog.js
@@ -99,4 +99,13 @@ router.post("/posts/:id/edit", async function (req, res) {
 
   res.redirect("/posts");
 });
+
+router.post("/posts/:id/delete", async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const result = await db
+    .getDb()
+    .collection("posts")
+    .deleteOne({ _id: postId });
+  res.redirect("/posts");
+});
 module.exports = router;
