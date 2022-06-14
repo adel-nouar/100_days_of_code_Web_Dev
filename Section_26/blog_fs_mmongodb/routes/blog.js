2eed63aec1f73ee803e146924dafd1d69980b8a0
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-14 09:33:14 +0200

    [65] - blog mongdb - inserting new post

--- Section_26/blog_fs_mmongodb/routes/blog.js
+++ Section_26/blog_fs_mmongodb/routes/blog.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const mongodb = require("mongodb");
 
 const db = require("../data/database");
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
@@ -16,6 +18,28 @@ router.get("/new-post", async function (req, res) {
   const authors = await db.getDb().collection("authors").find().toArray();
   console.log(authors);
   res.render("create-post", { authors: authors });
+});
+
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.eamail,
+    },
+  };
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+  console.log(result);
+  res.redirect("/posts");
 });
 
 module.exports = router;
