7ae67f4e4de4dfc65c71d70f067ecec2ca1f9a00
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-14 23:48:43 +0200

    [67] - blog mongdb - expressJS Handling async code errors

--- Section_26/blog_fs_mmongodb/routes/blog.js
+++ Section_26/blog_fs_mmongodb/routes/blog.js
@@ -47,12 +47,20 @@ router.post("/posts", async function (req, res) {
   res.redirect("/posts");
 });
 
-router.get("/posts/:id", async function (req, res) {
-  const postId = req.params.id;
+router.get("/posts/:id", async function (req, res, next) {
+  let postId = req.params.id;
+
+  try {
+    postId = new ObjectId(postId);
+  } catch (error) {
+    // return res.status(404).render("404");
+    return next(error);
+  }
+
   const post = await db
     .getDb()
     .collection("posts")
-    .findOne({ _id: new ObjectId(postId) }, { summary: 0 });
+    .findOne({ _id: postId }, { summary: 0 });
   if (!post) {
     return res.status(404).render("404");
   }
