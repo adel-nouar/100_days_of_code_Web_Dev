aa9eb163d23f48e1aadf49ccd694db4c2aa48417
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-19 13:39:18 +0200

    [69] - Updating DOM based on the response

--- Section_28/public/scripts/comments.js
+++ Section_28/public/scripts/comments.js
@@ -1,10 +1,29 @@
 const loadCommentsBtnElement = document.getElementById("load-comments-btn");
+const commentsSectionElement = document.getElementById("comments");
+
+function createCommentsList(comments) {
+  const commentListElement = document.createElement("ol");
+
+  for (const comment of comments) {
+    const commentElement = document.createElement("li");
+    commentElement.innerHTML = `
+      <article class="comment-item">
+        <h2>${comment.title}</h2>
+        <p>${comment.text}</p>
+      </article>
+    `;
+    commentListElement.appendChild(commentElement);
+  }
+  return commentListElement;
+}
 
 async function fetchCommentsForPost() {
   const postId = loadCommentsBtnElement.dataset.postid;
   const response = await fetch(`/posts/${postId}/comments`);
   const responseData = await response.json();
-  console.log(responseData);
+
+  const commentListElements = createCommentsList(responseData);
+  commentsSectionElement.appendChild(commentListElements);
 }
 
 loadCommentsBtnElement.addEventListener("click", fetchCommentsForPost);
