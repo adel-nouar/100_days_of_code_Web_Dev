6f413523e2ab14ebafcc58d1edafa06839d9dfbb
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-20 21:38:28 +0200

    [70] - Basic SignUp functionality

---
+++ Section_29/routes/demo.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const db = require("../data/database");
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.render("welcome");
+});
+
+router.get("/signup", function (req, res) {
+  res.render("signup");
+});
+
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData["confirm-email"];
+  const enteredPassword = userData.password;
+
+  const user = {
+    email: enteredEmail,
+    password: enteredPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
+});
+
+router.post("/login", async function (req, res) {});
+
+router.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {});
+
+module.exports = router;
