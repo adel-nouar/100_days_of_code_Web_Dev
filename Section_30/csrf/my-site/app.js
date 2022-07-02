c1f4398729cfee626d2144df30d86560c626b75f
Adel NOUAR <adel.nouar@protonmail.com>
2022-07-02 21:54:10 +0200

    [74] - Implementing better CSRF protection with csurf

--- Section_30/csrf/my-site/app.js
+++ Section_30/csrf/my-site/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const mongodbStore = require("connect-mongodb-session");
+const csrf = require("csurf");
 
 const db = require("./data/database");
 const demoRoutes = require("./routes/demo");
@@ -35,6 +36,8 @@ app.use(
     },
   })
 );
+
+app.use(csrf());
 
 app.use(async function (req, res, next) {
   const user = req.session.user;
