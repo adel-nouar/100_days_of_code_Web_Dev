c1147b479902e7afa72c15713c0112b04336b048
Adel NOUAR <adel.nouar@protonmail.com>
2022-07-06 16:26:33 +0200

    [77] - MVC: post controllers

--- Section_31/models/post.js
+++ Section_31/models/post.js
@@ -14,6 +14,24 @@ class Post {
     }
   }
 
+  static async fetchAll() {
+    const posts = await db.getDb().collection("posts").find().toArray();
+    return posts;
+  }
+
+  async fetch() {
+    if (!this.id) {
+      return;
+    }
+
+    const postDocument = await db
+      .getDb()
+      .collection("posts")
+      .findOne({ _id: this.id });
+    this.title = postDocument.title;
+    this.content = postDocument.content;
+  }
+
   async save() {
     let result;
     if (this.id) {
