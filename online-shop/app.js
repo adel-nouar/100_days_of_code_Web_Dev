cf40440bd081b0e75fbbcd52297a9156096e482d
Adel NOUAR <adel.nouar@protonmail.com>
2022-07-25 16:27:55 +0200

    [81]  Adding CSRF protection

--- online-shop/app.js
+++ online-shop/app.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const express = require("express");
+const csrf = require("csurf");
 
 const db = require("./data/database");
+const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
 
 const authRoutes = require("./routes/auth.routes");
 
@@ -12,7 +14,8 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-
+app.use(csrf());
+app.use(addCsrfTokenMiddleware);
 app.use(authRoutes);
 
 db.connectToDatabase()
