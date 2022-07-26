a15dc8d520d5b5cf7ceaa2a20176445434add3a1
Adel NOUAR <adel.nouar@protonmail.com>
2022-07-26 11:00:57 +0200

    [82] authentification + user login

--- online-shop/app.js
+++ online-shop/app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+
 const express = require("express");
 const csrf = require("csurf");
 const expressSession = require("express-session");
@@ -7,7 +8,10 @@ const createSessionConfig = require("./config/session");
 const db = require("./data/database");
 const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
+const checkAuthStatusMiddleware = require("./middlewares/check-auth");
 const authRoutes = require("./routes/auth.routes");
+const productsRoutes = require("./routes/products.routes");
+const baseRoutes = require("./routes/base.routes");
 
 const app = express();
 
@@ -21,8 +25,13 @@ const sessionConfig = createSessionConfig();
 
 app.use(expressSession(sessionConfig));
 app.use(csrf());
+
 app.use(addCsrfTokenMiddleware);
+app.use(checkAuthStatusMiddleware);
+
+app.use(baseRoutes);
 app.use(authRoutes);
+app.use(productsRoutes);
 
 app.use(errorHandlerMiddleware);
 
--- online-shop/models/user.model.js
+++ online-shop/models/user.model.js
@@ -14,6 +14,10 @@ class User {
     };
   }
 
+  getUserWithSameEmail() {
+    return db.getDb().collection("users").findOne({ email: this.email });
+  }
+
   async signup() {
     const hashedPassword = await bcrypt.hash(this.password, 12);
     await db.getDb().collection("users").insertOne({
@@ -22,6 +26,10 @@ class User {
       name: this.name,
       address: this.address,
     });
+  }
+
+  hasMatchingPassword(hashedPassword) {
+    return bcrypt.compare(this.password, hashedPassword);
   }
 }
 
